09b9fd84234d6f9f1ef22445769dddd621621e7b
kiminhye <alfn7511@gmail.com>
2021-01-03 01:38:47 +0900

    init

---
+++ src/router/index.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Main from '@/view/main'
+import Worklist from '@/worklist/index'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'Worklist',
+      component: Worklist
+    },
+    {
+      path: '/main',
+      name: 'Main',
+      component: Main
+    }
+  ]
+})
