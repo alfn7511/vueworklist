b963da02847ba54482f2ac3215321a6e6672add1
kiminhye <alfn7511@gmail.com>
2021-01-04 10:52:15 +0900

    element-ui install

--- src/main.js
+++ src/main.js
@@ -4,10 +4,12 @@ import Vue from 'vue';
 import App from './App';
 import router from './router';
 import Fragment from 'vue-fragment';
+import ElementUI from 'element-ui';
 import '@/assets/scss/index.scss';
 
 Vue.config.productionTip = false;
 Vue.use(Fragment.Plugin);
+Vue.use(ElementUI);
 /* eslint-disable no-new */
 new Vue({
 	el: '#app',
--- src/view/main.vue
+++ src/view/main.vue
@@ -1,9 +1,16 @@
 <template>
 	<div>
-		main
+		<el-date-picker v-model="date" type="date" placeholder="Pick a day">
+		</el-date-picker>
 	</div>
 </template>
 
 <script>
-export default {};
+export default {
+	data() {
+		return {
+			date: '2020-01-02',
+		};
+	},
+};
 </script>
